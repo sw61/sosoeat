9453e08629eeca8edad906cfaf7e6b392767bde5
sang1 <dign0614@gmail.com>
2026-03-23 15:27:21 +0900

    docs: 스토리북 파일 위치 수정

--- src/components/common/footer/footer.stories.tsx
+++ src/components/common/footer/footer.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import { Footer } from './footer';
 
 const meta = {
-  title: 'Common/Footer',
+  title: 'components/common/footer',
   component: Footer,
   parameters: {
     layout: 'fullscreen',
