40a8fe92fff0a5a4f3c31a878ff3c5b2e3dbf2f1
cncn0069 <cncn0069@naver.com>
2026-03-25 16:24:24 +0900

    feat: pofile image fix

--- src/components/common/main-page-card/main-page-card.tsx
+++ src/components/common/main-page-card/main-page-card.tsx
@@ -130,16 +130,33 @@ export const MainPageCard = ({ meeting }: MainPageCardProps) => {
       </CardContent>
 
       <CardFooter className="gap-1.5 rounded-none border-t border-[#F9FAFB] bg-transparent px-4 pt-1.5 pb-4 shadow-none ring-0">
-        <Image
-          src={meeting.host.image}
-          alt={meeting.host.name}
-          width={32}
-          height={32}
-          className="border-sosoeat-gray-300 overflow-hidden rounded-full border object-cover"
-        />
-        <span className="text-base leading-6 font-semibold text-[#6B7280]">
-          {meeting.host.name}
-        </span>
+        {meeting.host && meeting.host.image ? (
+          <>
+            <Image
+              src={meeting.host.image}
+              alt={meeting.host.name}
+              width={32}
+              height={32}
+              className="border-sosoeat-gray-300 overflow-hidden rounded-full border object-cover"
+            />
+            <span className="text-base leading-6 font-semibold text-[#6B7280]">
+              {meeting.host.name}
+            </span>
+          </>
+        ) : (
+          <>
+            <Image
+              src="/icons/human-basic.svg"
+              alt="empty-user"
+              width={32}
+              height={32}
+              className="border-sosoeat-gray-300 overflow-hidden rounded-full border object-cover"
+            />
+            <span className="text-base leading-6 font-semibold text-[#6B7280]">
+              {meeting.host.name}
+            </span>
+          </>
+        )}
       </CardFooter>
     </Card>
   );
