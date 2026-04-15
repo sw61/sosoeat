745eb4f6e80f0f4dd2bbf0b0f48e42a79a2144cd
cncn0069 <54030154+cncn0069@users.noreply.github.com>
2026-04-15 15:43:34 +0900

    refactor: font sub-set으로 불러 들여와 로딩시 필요한 폰트만 받도록

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,15 +55,7 @@ export default async function RootLayout({
 
   return (
     <html lang="ko">
-      <head>
-        <link
-          rel="preload"
-          href="/fonts/PretendardVariable.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </head>
+      <head></head>
       <body className="flex min-h-screen min-w-[375px] flex-col overscroll-none">
         <Script src="https://accounts.google.com/gsi/client" strategy="lazyOnload" />
         <NuqsAdapter>
